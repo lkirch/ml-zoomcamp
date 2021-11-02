bc7f9282df7cfd86722c5e16d4130b500bd2fc00
lkirch <lkirch@users.noreply.github.com>
2021-11-02 10:54:50 -0400

    to test prediction on heroku

---
+++ midterm/predict_test_heroku.py
@@ -0,0 +1,17 @@
+import requests
+
+url='https://ml-lk-zoom-docker.herokuapp.com/predict'
+
+employee = {"satisfaction_level": 5.8, 
+            "last_eval": 0.7, 
+            "num_projects": 19,
+            "avg_monthly_hrs": 256,
+            "time_spent_at_company": 4,
+            "work_accident": 0,
+            "promotion_last_5_yrs": 0,
+            "job_category": "sales",
+            "salary_group": "low"
+            }
+response = requests.post(url, json=employee).json()
+
+print(response)
